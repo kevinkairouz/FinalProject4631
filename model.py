5b3daad37bc5255147c47689871d589fcefb8acd
kevinkairouz <KM.Kairouz@gmail.com>
2026-04-04 18:24:29 -0400

    switch algorithm to Gradient Boosting for more control over model learning

--- model.py
+++ model.py
@@ -1,8 +1,7 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
-from sklearn.linear_model import LinearRegression 
-from sklearn.ensemble import RandomForestRegressor 
-from sklearn.ensemble import GradientBoostingRegressor 
+from sklearn.linear_model import LinearRegression  
+from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass 
 import mysql.connector as sql 
@@ -14,49 +13,38 @@ class User:
     study_hours: float 
     sleep_hours: float 
     noti_per_day: int 
-    focus_score: float 
-    productivity_score: float 
-    addiction_high: bool  
-    addiction_mid: bool 
-    addicition_low: bool 
-     
+    focus_score: float
+    productivity_score: float
     
 
 df = pd.read_csv("social.csv")
 #print(df.isna().sum())
-df = df.dropna()   
-df = pd.get_dummies(df, columns=["addiction_level"]) 
+df = df.dropna()    
+df = df.drop(columns=["addiction_level"])  
+print(df.info())
 df["age"] = pd.to_numeric(df["age"], downcast="integer") 
 df["notifications_per_day"] = pd.to_numeric(df["notifications_per_day"], downcast="integer")
-
-print(df.info()) 
-print(df.sample().to_string())
-
-
 X = df.drop(columns=["productivity_score"])
-Y = df[["productivity_score", "focus_score"]] 
+Y = df["productivity_score"]
 
-X_train, X_test, Y_train, Y_test = train_test_split(X,Y,random_state=42)
-model = LinearRegression()
-model.fit(X_train,Y_train)
-#print(model.score(X_test,Y_test)) 
-# benchamarked at 87.5%
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,random_state=42, test_size=0.3)
+# model = LinearRegression() 
+model = GradientBoostingRegressor()
+model.fit(X_train,Y_train) 
+# print(df.info())
+print(model.score(X_test,Y_test)) 
+# benchamarked at 87.4%
 #in stock testing 
 
-def predict(user = User): 
-    features = [user.age, ] 
-
-    daily_screen_time: float 
-    study_hours: float 
-    sleep_hours: float 
-    noti_per_day: int 
-    focus_score: float 
-    productivity_score: float 
-    addiction_high: bool  
-    addiction_mid: bool 
-    addicition_low: bool
-
-    model.predict() 
+def predict(age, dst, smh, sth, slh, npd): 
+    # L = [u.age, u.daily_screen_time, u.study_hours, u.sleep_hours, u.noti_per_day, 
+    #      u.addiction_high, u.addiction_mid, u.addicition_low] 
+    udf = pd.DataFrame({"age": [age], "daily_screen_time": [dst], "social_media_hours": [smh], 
+                        "study_hours": [sth], "sleep_hours": [slh], "notifications_per_day": [npd]}) 
+     
+    return model.predict(udf)
 
 def sendPrediction():  
-    return "" 
+    return ""  
+
+
