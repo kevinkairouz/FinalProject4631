15435dba7951e817026a68087c95ca9db9856684
kevinkairouz <KM.Kairouz@gmail.com>
2026-04-04 01:11:49 -0400

    read csv and init flask backend

---
+++ app.py
@@ -0,0 +1,10 @@
+from flask import Flask 
+import mysql.connector as sql  
+
+app = Flask(__name__) 
+
+""" 
+TODO: 
+1: Make Route for sending predictions to frontend 
+2: Make helper function that sends our data to MySQL DB
+"""
--- model.py
+++ model.py
@@ -1,2 +1,11 @@
-import pandas as pd 
-from sklearn.ensemble import RandomForestClassifier
+import pandas as pd  
+import matplotlib.pyplot as plt
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn.ensemble import GradientBoostingClassifier 
+from sklearn.ensemble import VotingClassifier 
+
+
+df = pd.read_csv("prompts.csv")  
+#used to check for amount of null values in our Dataframe (there are zero) 
+# print(df.isna().sum())
+
