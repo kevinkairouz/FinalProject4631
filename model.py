d9ecd07c374172f0deaa0df23812ae1e50f4bc93
kevinkairouz <KM.Kairouz@gmail.com>
2026-04-05 03:01:05 -0400

    implemented database function and tested it

--- model.py
+++ model.py
@@ -1,11 +1,11 @@
 import pandas as pd 
 import matplotlib.pyplot as plt 
 from sklearn.linear_model import LinearRegression  
-from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.ensemble import GradientBoostingRegressor, RandomForestRegressor, AdaBoostRegressor
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass 
 import mysql.connector as sql 
-
+from sklearn.model_selection import GridSearchCV
 @dataclass 
 class User: 
     age: int 
@@ -28,11 +28,11 @@ X = df.drop(columns=["productivity_score"])
 Y = df["productivity_score"]
 
 X_train, X_test, Y_train, Y_test = train_test_split(X,Y,random_state=42, test_size=0.3)
-# model = LinearRegression() 
-model = GradientBoostingRegressor()
+model = GradientBoostingRegressor(n_estimators=150,learning_rate=0.1) 
 model.fit(X_train,Y_train) 
-# print(df.info())
-print(model.score(X_test,Y_test)) 
+
+# print(model.score(X_test,Y_test))  
+
 # benchamarked at 87.4%
 #in stock testing 
 
@@ -41,10 +41,27 @@ def predict(age, dst, smh, sth, slh, npd):
     #      u.addiction_high, u.addiction_mid, u.addicition_low] 
     udf = pd.DataFrame({"age": [age], "daily_screen_time": [dst], "social_media_hours": [smh], 
                         "study_hours": [sth], "sleep_hours": [slh], "notifications_per_day": [npd]}) 
-     
-    return model.predict(udf)
+    
+    """
+    productivity score
+    
+    """
+    res = model.predict(udf)
 
-def sendPrediction():  
-    return ""  
+def sendPrediction(age, dst, smh, sth, slh, npd, ps): 
+    db = sql.connect(host = "localhost", user = "root", password = "Dominics1", database ="finalProj") 
+    query = "insert into user_predictions (age, daily_screen_time, social_media_hours,study_hours,sleep_hours,notifications_per_day,productivity_score) values (%s,%s,%s,%s,%s,%s,%s)"
+    
+    
+    data = (age, dst, smh, sth, slh, npd, ps)
+    cursor = db.cursor() 
+    cursor.execute(query,data) 
+    db.commit() 
+    # db.close()
+
+    
+    
+    return "Success"  
 
 
+sendPrediction(25, 2.4, 4.5, 5.9, 4.0, 200, 70.5) 
