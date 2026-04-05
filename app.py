eb59767803e61a6baa0df41c64f24164d98d5b7c
kevinkairouz <KM.Kairouz@gmail.com>
2026-04-05 14:52:17 -0400

    Created frontend files

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request,jsonify
 from flask_cors import CORS, cross_origin
 import mysql.connector as sql 
-import model as m 
+import model.model as m 
 
 app = Flask(__name__) 
 CORS(app)
@@ -22,7 +22,6 @@ def makePrediction():
     user_study_hours = data["study_hours"] 
     user_sleep_hours = data["sleep_hours"] 
     user_notifications_per_day = data["notifications_per_day"] 
-
     pred = m.predict(user_age, user_daily_screen_time,user_social_media_hours,user_study_hours,user_sleep_hours,user_notifications_per_day)
     return jsonify({"productivity_score": pred}) 
 
