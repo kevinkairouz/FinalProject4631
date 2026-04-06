0a325a820f97e9f11eadb33900eeec3fefb8dd9c
kevinkairouz <KM.Kairouz@gmail.com>
2026-04-06 17:16:06 -0400

    wrote comments to guide team on analytics and visualization course of action

--- model/model.py
+++ model/model.py
@@ -46,3 +46,36 @@ def sendPrediction(age, dst, smh, sth, slh, npd, ps):
 
 #for testing purpose 
 # sendPrediction(25, 2.4, 4.5, 5.9, 4.0, 200, 70.5) 
+
+#TODO: 
+#1: Have a scatter plot to show correlation whether it 
+#is positive or negative for the following relationships  
+#make it a subplot that has 4 charts 
+"""
+Sleep Hours => Productivity Score 
+Study Hours => Productivity Score 
+Notifications Per Day => Social Media Time  
+Age => Social Media Time 
+
+""" 
+
+#2: Have a histogram for the ages to show diversity within the data ie meaning we can 
+# defend our work  
+
+""" 
+If we have solid diversity within our data we prove not only is our data of such high quality, 
+but we are able to show that it is indeed reliable data that is free of bias, therefore data quality 
+is not a concern for our model 
+
+""" 
+
+
+#3: Create a bar chart that indicate what our averages are in the data, ie what is the avg for 
+#age, social media time, and we can go further to say for each age range what is the average time 
+
+
+"""
+This gives us an oppurtunity to showcase analytical processes
+
+
+"""
