5b5f583ec94491a9489905614325d6469ac2b508
kevinkairouz <KM.Kairouz@gmail.com>
2026-04-08 00:50:50 -0400

    started implementationof visualization

--- model/analysis.py
+++ model/analysis.py
@@ -1,2 +1,11 @@
 import pandas as pd 
-import matplotlib.pyplot as plt 
+import matplotlib.pyplot as plt  
+
+
+df = pd.read_csv("../data/social.csv").sample(50) 
+
+plt.scatter(df["sleep_hours"], df["productivity_score"])
+plt.scatter(df["study_hours"], df["productivity_score"])
+
+
+plt.show()
