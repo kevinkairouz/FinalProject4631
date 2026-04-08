9562006712ed8fa9dd0ebb4ea1e8f2e2fc569acc
kevinkairouz <KM.Kairouz@gmail.com>
2026-04-08 10:42:54 -0400

    started visualization

--- model/analysis.py
+++ model/analysis.py
@@ -2,10 +2,18 @@ import pandas as pd
 import matplotlib.pyplot as plt  
 
 
-df = pd.read_csv("../data/social.csv").sample(50) 
+df = pd.read_csv("../data/social.csv").sample(50)  
 
-plt.scatter(df["sleep_hours"], df["productivity_score"])
-plt.scatter(df["study_hours"], df["productivity_score"])
+fig, axis = plt.subplots(2, 2) 
 
+axis[0,0].scatter(df["sleep_hours"], df["productivity_score"]) 
 
 plt.show()
+
+# Correlation of sleeping and productivity 
+# Correlation of studying and productivity
+# plt.scatter(df["sleep_hours"], df["productivity_score"])
+# plt.scatter(df["study_hours"], df["productivity_score"])
+
+
+# plt.show()
