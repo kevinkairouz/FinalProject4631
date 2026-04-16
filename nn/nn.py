8ef58f20813f89940b290c7c73395869adcefabb
kevinkairouz <KM.Kairouz@gmail.com>
2026-04-16 00:26:08 -0400

    Created folder for possibly creating a neural network in pytorch FCNN fully connected nn

---
+++ nn/nn.py
@@ -0,0 +1,11 @@
+import torch 
+import torch.nn 
+import torch.functional as f 
+from sklearn.model_selection import train_test_split 
+from torch.utils.data import DataLoader 
+
+#Possible that pytorch nn (fully connected/linear nn is made for testing/implementation purpose) 
+
+def main(): 
+    return None 
+
