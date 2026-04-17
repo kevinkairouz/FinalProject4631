9b70662394faf74ad93b2be57548537ced412991
kevinkairouz <KM.Kairouz@gmail.com>
2026-04-16 23:40:15 -0400

    started building a nn in pytorch

--- nn/nn.py
+++ nn/nn.py
@@ -4,8 +4,12 @@ import torch.functional as f
 from sklearn.model_selection import train_test_split 
 from torch.utils.data import DataLoader 
 
+
 #Possible that pytorch nn (fully connected/linear nn is made for testing/implementation purpose) 
 
-def main(): 
-    return None 
-
+nn = torch.nn.Sequential(
+    torch.nn.Linear(6, 8), 
+    torch.nn.ReLU(), 
+    
+    
+)
