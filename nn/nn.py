2d0bb66f6a9f0dcfd19a7acb0e739f9c4ee6d2a9
kevinkairouz <KM.Kairouz@gmail.com>
2026-04-18 00:59:42 -0400

    modified todo

--- nn/nn.py
+++ nn/nn.py
@@ -8,6 +8,16 @@ import numpy as np
 
 df = pd.read_csv("../data/social.csv") 
 
+df = df.dropna()    
+df = df.drop(columns=["addiction_level", "focus_score"])  
+df["age"] = pd.to_numeric(df["age"], downcast="integer") 
+df["notifications_per_day"] = pd.to_numeric(df["notifications_per_day"], downcast="integer")
+X = df.drop(columns=["productivity_score"])
+Y = df["productivity_score"]
+
+
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y,random_state=42, test_size=0.3) 
+
 
 
 #Possible that pytorch nn (fully connected/linear nn is made for testing/implementation purpose) 
