24122ea881ba189ddca9896a8897b07f8b07eb3b
kevinkairouz <KM.Kairouz@gmail.com>
2026-04-20 22:31:26 -0400

    Added ability to make prediciton in console.py

--- model/console.py
+++ model/console.py
@@ -1,8 +1,28 @@
 import analysis 
-import histogram
+import histogram 
+from neuralNetwork import nnPrediction 
+from model import model
 
 def main(): 
     analysis  
     histogram 
     
-main()
+main() 
+
+
+a = int(input("Enter Age: ")) 
+daily_screen_hours = float(input("Enter Daily Screen Hours: ")) 
+social_media_hours = float(input("Enter Social Media Hours: ")) 
+study_hours = float(input("Enter Study Hours: "))
+sleep_hours = float(input("Enter Sleep Hours: ")) 
+notis = int(input("Enter Notifications: ")) 
+
+#Gradient Boosting Model 
+print("Gradient Boosting Prediction: ")
+print(model.predict(a, daily_screen_hours, social_media_hours, study_hours, sleep_hours, notis)) 
+
+
+#Neural Network 
+print("Neural Network Prediction: ")
+print(nnPrediction(a, daily_screen_hours, social_media_hours, study_hours, sleep_hours, notis))
+
