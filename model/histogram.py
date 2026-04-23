7536a301468ab5f75326824d31b05d82de6f20a7
kevinkairouz <KM.Kairouz@gmail.com>
2026-04-23 18:29:10 -0400

    Renamed model to models and added histogram

--- model/histogram.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd 
-import matplotlib.pyplot as plt 
-
-df = pd.read_csv("../data/social.csv") 
-
-plt.hist(df["age"], color="green")  
-plt.title("Age Distribution (HISTOGRAM)")
-plt.show()
